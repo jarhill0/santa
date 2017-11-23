d80b3fce0b920238ac605fe6f046ebddbed0b516
jarhill0 <jarhill0@gmail.com>
2017-11-22 22:29:36 -0800

    📇 Add setup.py and setup.cfg

---
+++ setup.py
@@ -0,0 +1,17 @@
+from distutils.core import setup
+
+setup(
+    name='santa',
+    packages=['santa'],
+    version='0.1',
+    description='Assign a secret santa over email',
+    author='jarhill0',
+    author_email='jarhill0@gmail.com',
+    url='https://github.com/jarhill0/santa/',
+    download_url='https://github.com/jarhill0/santa/archive/0.1.tar.gz',
+    keywords=['secret', 'santa', 'email'],
+    classifiers=[],
+    install_requires=[
+        'yagmail >=0.10.209',
+    ]
+)
