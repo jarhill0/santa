3c4e291e29ced49276343049bf7f42423e13753c
jarhill0 <jarhill0@gmail.com>
2017-11-22 22:32:07 -0800

    🔖 tag version 0.1.1
    add .gitignore

--- setup.py
+++ setup.py
@@ -3,12 +3,12 @@ from distutils.core import setup
 setup(
     name='santa',
     packages=['santa'],
-    version='0.1',
+    version='0.1.1',
     description='Assign a secret santa over email',
     author='jarhill0',
     author_email='jarhill0@gmail.com',
     url='https://github.com/jarhill0/santa/',
-    download_url='https://github.com/jarhill0/santa/archive/0.1.tar.gz',
+    download_url='https://github.com/jarhill0/santa/archive/0.1.1.tar.gz',
     keywords=['secret', 'santa', 'email'],
     classifiers=[],
     install_requires=[
